95380bff8f5d9a57e56becc4a238188be2418547
Siyuan <siyuan.vince.zhao@gmail.com>
2016-02-03 13:35:41 -0500

    new stuff

--- JavaDL/src/dl/data/DataPreprocess.java
+++ JavaDL/src/dl/data/DataPreprocess.java
@@ -1,6 +1,8 @@
 package dl.data;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,7 +18,7 @@ import java.util.Set;
 public class DataPreprocess {
 
 	public static void main(String[] args) {
-		preprocessData("problem_logs_2015.csv");
+		preprocessData();
 		
 	}
 	
@@ -30,10 +32,10 @@ public class DataPreprocess {
 		return -1;
 	}
 	
-	public static void preprocessData(String filePath) {
+	public static void preprocessData() {
 		Set<Integer> set = new HashSet<>();
-		//String csvFile = "/home/siyuan/Downloads/problem_logs_2015.csv";
-		DataPreprocess.class.getResourceAsStream(filePath);
+		String csvFile = "/Users/Siyuan/Documents/workspace/dl/raw_data/problem_logs_2015.csv";
+		//DataPreprocess.class.getResourceAsStream(filePath);
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
@@ -43,13 +45,24 @@ public class DataPreprocess {
 			StudentLevelData sld = null;
 			list = new ArrayList<>();
 			br = new BufferedReader(new InputStreamReader
-					(DataPreprocess.class.getResourceAsStream(filePath)));
+					(new FileInputStream(new File(csvFile))));
+			int count = 0;
 			while ((line = br.readLine()) != null) {
+				/*
+				if(count == 0) {
+					count++;
+					continue; //skip the header
+				}*/
 				// use comma as separator
 				String[] columns = line.split(cvsSplitBy);
 				int studentId = Integer.valueOf(columns[0]);
-				int problemSetId = Integer.valueOf(columns[2]);
-				set.add(problemSetId);
+				int problemSetId = 0;
+				try {
+					problemSetId = Integer.valueOf(columns[2]);
+					set.add(problemSetId);
+				} catch(Exception e) {
+					continue;
+				}
 				float floatCorrectness = Float.valueOf(columns[3]);
 				int correctness = 0;
 				if(floatCorrectness == 1) {
@@ -86,6 +99,25 @@ public class DataPreprocess {
 				}
 			}
 		}
+		System.out.println("The number of skills is " + set.size());
+		//print out the skill mapping
+		Iterator<Integer> it = set.iterator();
+		
+		try {
+			FileWriter writer = new FileWriter("/Users/Siyuan/Documents/workspace/dl/JavaDL/skill_mapping.csv");
+			//first write down the num of skills in the dataset
+			writer.append("The number of skill builders is " + set.size() + "\n");
+			int index = 0;
+			while(it.hasNext()) {
+				int skill_id = it.next();
+				writer.append(index + "," + skill_id+"\n");
+				index += 1;
+			}
+			writer.flush();
+			writer.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 		// shuffle the list
 		long seed = System.nanoTime();
 		Collections.shuffle(list, new Random(seed));
@@ -99,7 +131,7 @@ public class DataPreprocess {
 		// write to files
 		try {
 			//get file path
-			FileWriter writer = new FileWriter("/home/siyuan/programs/eclipse/workspace/DL/train.csv");
+			FileWriter writer = new FileWriter("/Users/Siyuan/Documents/workspace/dl/JavaDL/builder_train.csv");
 			while(iter.hasNext()) {
 				StudentLevelData item = iter.next();
 				writer.append(item.num + "\n");
@@ -119,7 +151,7 @@ public class DataPreprocess {
 		
 		iter = testList.iterator();
 		try {
-			FileWriter writer = new FileWriter("/home/siyuan/programs/eclipse/workspace/DL/test.csv");
+			FileWriter writer = new FileWriter("/Users/Siyuan/Documents/workspace/dl/JavaDL/builder_test.csv");
 			while(iter.hasNext()) {
 				StudentLevelData item = iter.next();
 				writer.append(item.num + "\n");
