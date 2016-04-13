8a56e2c1cea0ec505cea53b105e0707e8f9d73ef
Siyuan Zhao <siyuan.vince.zhao@gmail.com>
2016-04-13 12:30:50 -0400

    Update raw_data.sql

--- raw_data/raw_data.sql
+++ raw_data/raw_data.sql
@@ -21,3 +21,37 @@ where ca.sequence_id in (
 ) and pl.start_time::date >= '2015-01-01' and pl.start_time::date < '2015-12-01' and pl.correct is not null
 and pl.original = 1
 order by user_id, pl.id
+
+-- get problem logs for 100 skill builders with two features (student prior knowledge, correctness on problem set level)
+select pl.user_id, pl.id as log_id, ca.sequence_id, pl.correct, sac.correctness as prior_knowledge, psc.correctness as ps_correctness
+from problem_logs pl
+left join class_assignments ca on ca.id = pl.assignment_id
+left join 
+(select plc.user_id, correct_count::float/total_count as correctness, plt.total_count
+from
+(select user_id, count(*) as correct_count
+from problem_logs
+where original = 1 and correct = 1
+group by user_id) plc
+join (select user_id, count(*) as total_count
+from problem_logs
+where original = 1 and correct is not null
+group by user_id) plt on plt.user_id = plc.user_id) sac on sac.user_id = pl.user_id
+join 
+(select plc.sequence_id, correct_count::float/total_count as correctness, plt.total_count
+from
+(select ca.sequence_id, count(*) as correct_count
+from problem_logs pl
+join class_assignments ca n ca.id = pl.assignment_id
+where original = 1 and correct = 1
+group by ca.sequence_id) plc
+join (select ca.sequence_id, count(*) as total_count
+from problem_logs pl
+join class_assignments ca on ca.id = pl.assignment_id
+where original = 1 and correct is not null
+group by ca.sequence_id) plt on plt.sequence_id = plc.sequence_id) psc on psc.sequence_id = ca.sequence_id
+where ca.sequence_id in (
+5968,11898,6921,5969,37570,37055,6022,10195,5961,11831,7159,10265,5945,11899,7014,7157,19610,7012,8741,8949,10597,11893,11829,6009,8585,6891,5965,9245,5976,6473,10264,7196,14211,6039,14247,10763,7020,7195,39162,37374,6018,11836,9424,10765,7160,9180,7166,6910,5962,12450,9423,9222,7155,5918,10833,8946,19362,7179,7165,31277,21257,9428,6849,13935,13731,7185,37980,6851,26902,6937,14168,7158,37876,10767,31825,10293,6913,15528,9244,6465,164496,6402,6943,5898,14442,7184,37002,7183,204037,39885,7035,236309,8928,7182,7167,7156,24173,7192,31260,8864
+) and pl.start_time::date >= '2015-01-01' and pl.start_time::date < '2015-12-01' and pl.correct is not null
+and pl.original = 1
+order by user_id, pl.id
